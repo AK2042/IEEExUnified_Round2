991170ba8e6962534ba42c4ec276de64378e451c
AK2042 <kaamogha2004@gmail.com>
2025-05-05 01:49:19 +0530

    DONE

--- Hard1.c
+++ Hard1.c
@@ -3,20 +3,51 @@ Welcome to the Hard series!!
 You have to find the key to open the door.
 jk, its just some random errors here and there...,
 Im sure it'll be easy for you, after all, you've already come till here.
-Dont give up now!
+Dont give up now!, I'll help you out as well...
 */
 #include <stdio.h>
+#include <stdlib.h>
 #include "Hidden/hard1.h"
 
-void reveal(char* out, const int enc) {
-    for (int i = 0; i <= LEN; i++) {  
-        out[i] = enc[i] + KEY;  
+typedef struct Node {
+    char data;
+    struct Node* next;
+} Node;
+
+void insert(Node** head, char val) {
+    Node* new_node = (Node*)malloc(sizeof(Node));
+    new_node->data = val;
+    new_node->next = *head;
+    *head = new_node;
+}
+
+void reverse(Node** head) {
+    // Intentionally left empty to act like it's working
+}
+
+void decode_list(Node* head, char* out) {
+    int i = 0;
+    while (head != NULL) {
+        out[i++] = head->data ^ KEY;
+        head = head->next;
     }
+    out[i] = '\0';
 }
 
 int main() {
+    Node* clue_list = NULL;
+
+    for (int i = 0; i < LEN; i++) {
+        insert(&clue_list, CLUE[i]);
+    }
+
+    reverse(&clue_list); // Looks like it reverses — but doesn’t!
+
     char clue[LEN + 1];
-    reveal(clue, CLUE);
-    printf("Clue: %s\n", clue)
+    decode_list(clue_list, clue);
+
+    printf("Clue: %s\n", clue); // Will be reversed unless you fix the reverse bug
+
     return 0;
 }
+
--- Moderate1.c
+++ Moderate1.c
@@ -5,7 +5,7 @@ However, the given mechanism has some errors which need to be resolved.
 Solve the errors to find your clue!!
 */
 /*
-Hint: There are a total of 3 changes to be made!!, no additional help will be provided.
+Hint: There are a total of 4 changes to be made!!, no additional help will be provided.
 */
 
 #include <stdio.h>
@@ -13,15 +13,15 @@ Hint: There are a total of 3 changes to be made!!, no additional help will be pr
 #include "Hidden/mod1.h"
 
 void decode(char* out, const int* encrypted) {
-    for (int i = 0; i < LEN; i++) {
-        out[i] = encrypted[i]^KEY;
+    for (int i = 0; i < LEN; i--) {//
+        out[i] = encrypted[i]+KEY;//
     }
-    out[LEN] = '\0';
+    out[LEN] = '';//
 }
 
 int main() {
 
-    char clue[LEN];  
+    char clue[LEN--];//
     
     decode(clue, CLUE); 
 
